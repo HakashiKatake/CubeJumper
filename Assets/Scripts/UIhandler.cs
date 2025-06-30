ecec7f8d76ba7512eed1f4c618de5057a5ba6693
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-06-30 18:01:35 +0530

    Initial commit

---
+++ Assets/Scripts/UIhandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIhandler : MonoBehaviour 
+{
+    public void ReplayGame()
+    {
+        // Clear any persistent data if needed
+        Time.timeScale = 1f; // Reset time scale in case it was paused
+        
+        // Reload the current scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
